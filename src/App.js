4a193afb2525c7a786a086d4c431f83ce6e54ed1
alisohailshaikh <alksohailshaikh@gmail.com>
2023-05-06 23:28:43 +0500

    eh
    New Project

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Login } from './login';
+import { Register } from './register';
+import Title_Page from './title_page';
+import { Dashboard } from './Components/dashboard';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { LoginU } from './loginU';
+import { RegisterU } from './signupU';
+import {EventHallList} from './MyVenues';
+import Member1 from './Team';
+import {SearchBar} from './SearchBar';
+import LocationsList from './locationsList';
+import Testing from './testing';
+import LocationDetails from './LocationDetails';
+
+function App() {
+  const [currForm,setCurrForm]=useState('login');
+
+  const toggleForm= (formName) => {
+    setCurrForm(formName)
+  }
+
+  return (
+      <div className="App">
+      <Router>
+      <Routes>
+      <Route exact path='/' element={<Title_Page/>}/>
+      <Route exact path='/DB' element={<Dashboard/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Register/>}/>
+      <Route path='/loginU' element={<LoginU/>}/>
+      <Route path='/signupU' element={<RegisterU/>}/>
+      <Route path='/MyVenues' element={<EventHallList/>}/>
+      <Route path='/Team' element={<Member1/>}/>
+      <Route path='location' element={<LocationsList/>}/>
+      <Route path='testing' element={<Testing/>}/>
+      <Route exact path='/SearchBar' element={<SearchBar/>}/>
+      <Route path="location/:id" element={<LocationDetails/>} />
+
+      </Routes>
+    </Router>
+    </div>
+
+  );
+}
+
+export default App;
+
+// {/* {
+//         currForm == "login" ? <Login/>: <Register/>
+//       } */}
---
+++ src/Components/dashboard.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { useState, useEffect } from "react"
+// import form from "./iamges/Form.png"
+import sideBar from "./SideBardata"
+import {Link} from 'react-router-dom'
+import './Dashboard.css'
+
+export const Dashboard = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isSmallScreen = windowWidth <= 768; // set breakpoint as desired
+
+  return (
+    <>
+    <h1 className="dashboard-heading">Welcome</h1>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
+    <sideBar/>
+    <div className="dashboard-container">
+      <div className="cards-container">
+        <div className="card">
+          <h2>Total Users</h2>
+          <p>500</p>
+        </div>
+        <div className="card">
+          <h2>Revenue</h2>
+          <p>$50,000</p>
+        </div>
+        <div className="card">
+          <h2>Orders</h2>
+          <p>200</p>
+        </div>
+        <div className="card">
+          <h2>Product Views</h2>
+          <p>5,000</p>
+        </div>
+        <button className="logout-button">
+          <Link to = '/'>
+            Logout
+          </Link>
+          </button>
+      </div>
+    </div>
+    <div className="hs-icon">HS</div>
+      <div className="user-name">Hamza Shariq</div>
+
+    </>
+  );
+}
+
+
+
+
